f6d6419f332aa9637800d7bc2ee1abd509209c7e
Srthelcat <daniilermakov597@gmail.com>
2026-04-14 15:06:34 +0300

    Refactor book file handling and output display
    Refactor file reading to use the current script directory and improve output formatting.

--- 3.py
+++ 3.py
@@ -1,17 +1,22 @@
+import os
+
 def parse_line(line):
-        parts = line.split('|')
-        return [parts[0], parts[1], parts[2], int(parts[3]), float(parts[4])]
+    parts = line.split('|')
+    return [parts[0], parts[1], parts[2], int(parts[3]), float(parts[4])]
 
 
 def get_books(file_name):
-    with open(file_name, 'r', encoding='utf-8') as f:
+    # Получаем путь к текущей директории скрипта
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(current_directory, file_name)
+
+    with open(file_path, 'r', encoding='utf-8') as f:
         content = f.read()
 
     lines = content.strip().split('\n')
 
     return list(map(parse_line, lines[1:]))
 
-get_books("books.csv")
 
 def filtered_books(books, keyword):
     matches = filter(lambda b: keyword.lower() in b[1].lower(), books)
@@ -20,15 +25,19 @@ def filtered_books(books, keyword):
         matches
     ))
 
+
 books = get_books("books.csv")
 result = filtered_books(books, "python")
-print(result)
+
 def books_total(books):
     return list(map(
         lambda b: (b[0], b[3] * b[4]),
         books
     ))
 
-books = get_books("books.csv")
+print(books)
+print('--------------------------')
+print(result)
+print('--------------------------')
 totals = books_total(books)
 print(totals)
